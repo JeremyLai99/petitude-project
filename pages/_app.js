509b27aa4f32cfc0fa618385bdcfba4bed672f09
JeremyLai99 <jeremylai0728@gmail.com>
2024-07-05 15:32:38 +0800

    [accordion display issue]

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import "@/styles/pi-index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+
+
